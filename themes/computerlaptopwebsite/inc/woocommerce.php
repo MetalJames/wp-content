0e98472541a7de5db5128c197b9320e2342c63b5
Volodymyr <volodyaruzhak@gmail.com>
2021-02-25 16:56:48 +0200

    Done some work with cart

--- themes/computerlaptopwebsite/inc/woocommerce.php
+++ themes/computerlaptopwebsite/inc/woocommerce.php
@@ -1,10 +1,10 @@
 <?php
 add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 1 );
 
-function computerlaptopwebsite_woocommerce_single_product_summury_subtitle() {
-    echo 'Look at the amazing price!!!';
-}
-add_action( 'woocommerce_single_product_summary', 'computerlaptopwebsite_woocommerce_single_product_summury_subtitle', 2 );
+// function computerlaptopwebsite_woocommerce_single_product_summury_subtitle() {
+//     echo 'Look at the amazing price!!!';
+// }
+// add_action( 'woocommerce_single_product_summary', 'computerlaptopwebsite_woocommerce_single_product_summury_subtitle', 2 );
 
 // function computerlaptopwebsite_woocommerce_before_add_to_cart_quantity_test() {
 //     echo '<span style="color:red">Look at the amazing price!!!</span>';
